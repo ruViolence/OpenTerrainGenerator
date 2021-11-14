0c3c160f0317701d36e7d9a9d9703e2f6615ae1d
authvin <adragondil@live.com>
2021-11-14 19:23:17 +0100

    1.16.5 - 0.1.1: Removed MajorVersion from biome resource location strings

--- common/common-util/src/main/java/com/pg85/otg/util/biome/OTGBiomeResourceLocation.java
+++ common/common-util/src/main/java/com/pg85/otg/util/biome/OTGBiomeResourceLocation.java
@@ -21,7 +21,7 @@ public class OTGBiomeResourceLocation implements IBiomeResourceLocation
 		this.presetMajorVersion = presetMajorVersion;
 		this.presetFolder = presetFolder.toFile().getName();
 		this.presetShortName = presetShortName != null && presetShortName.trim().length() > 0 ? presetShortName : this.presetFolder;		
-		this.presetRegistryName = this.presetShortName.toLowerCase().trim().replaceAll("[^a-z0-9/_-]", "_") + (presetMajorVersion == 0 ? "" : BIOME_RESOURCE_LOCATION_SEPARATOR + presetMajorVersion);
+		this.presetRegistryName = this.presetShortName.toLowerCase().trim().replaceAll("[^a-z0-9/_-]", "_");
 		this.biomeName = biomeName.toLowerCase().trim().replaceAll("[^a-z0-9/_-]", "_");
 		this.resourceName = resourceName;
 	}
