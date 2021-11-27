8dd685190595147beb53cf23cdae1c9bd56539e2
PG85 <rik_lubking@hotmail.com>
2021-11-27 03:31:53 +0100

    1.16.5-0.1.5: Fix /otg exportbo4data

--- common/common-customobject/src/main/java/com/pg85/otg/customobject/bo4/BO4Config.java
+++ common/common-customobject/src/main/java/com/pg85/otg/customobject/bo4/BO4Config.java
@@ -1367,7 +1367,6 @@ public class BO4Config extends CustomObjectConfigFile
 		stream.writeInt(this.bo4DataVersion);
 		// Version 3 added fixedRotation		
 		StreamHelper.writeStringToStream(stream, this.fixedRotation == null ? null : this.fixedRotation.toString());
-		stream.writeInt(this.bo4DataVersion);
 		stream.writeInt(this.minimumSizeTop);
 		stream.writeInt(this.minimumSizeBottom);
 		stream.writeInt(this.minimumSizeLeft);
