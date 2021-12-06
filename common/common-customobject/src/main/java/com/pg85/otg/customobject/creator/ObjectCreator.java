ce200c5dfbcda6ebbe23def48b772ae37c1691a1
PG85 <rik_lubking@hotmail.com>
2021-12-06 16:36:12 +0100

    1.16.5-0.1.7: Minor cleanup

--- common/common-customobject/src/main/java/com/pg85/otg/customobject/creator/ObjectCreator.java
+++ common/common-customobject/src/main/java/com/pg85/otg/customobject/creator/ObjectCreator.java
@@ -27,7 +27,6 @@ import com.pg85.otg.interfaces.IModLoadedChecker;
 import com.pg85.otg.util.ChunkCoordinate;
 import com.pg85.otg.util.logging.LogCategory;
 import com.pg85.otg.util.logging.LogLevel;
-import com.pg85.otg.util.materials.LocalMaterials;
 import com.pg85.otg.util.nbt.LocalNBTHelper;
 import com.pg85.otg.util.gen.LocalWorldGenRegion;
 import com.pg85.otg.util.materials.LocalMaterialData;
